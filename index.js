4cca1b322acb9c1e6b3184dfef922db847d79954
Luke Edwards <luke.edwards05@gmail.com>
2017-02-12 01:24:26 -0800

    add module logic

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
+
+const Script = require('vm').Script;
+const reqLike = require('require-like');
+
+const box = {};
+
+module.exports = function (filepath, contents) {
+	if (typeof filepath !== 'string') {
+		throw new TypeError(`Filepath must be a string; got ${typeof filepath}`);
+	} else if (typeof contents !== 'string') {
+		throw new TypeError(`Contents must be a string; got ${typeof contents}`);
 	}
-
-	opts = opts || {};
-
-	return input + ' & ' + (opts.postfix || 'rainbows');
+	// setup mock env
+	Object.assign(box, global);
+	box.module = {exports};
+	box.exports = exports;
+	box.require = reqLike(filepath);
+	// define a script
+	const scr = new Script(contents);
+	// `eval()` new content elsewhere
+	scr.runInNewContext(box);
+	// return new `tasks` object
+	return box.module.exports;
 };
