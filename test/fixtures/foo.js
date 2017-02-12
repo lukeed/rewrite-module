26c0e43f274516b458578da18c0b42529a072e93
Luke Edwards <luke.edwards05@gmail.com>
2017-02-12 01:25:23 -0800

    define tests

---
+++ test/fixtures/foo.js
@@ -0,0 +1,5 @@
+export var hello = 'world';
+
+export function foo(a, b) {
+	return a + b;
+}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,42 @@
-import test from 'ava';
-import fn from './';
+const read = require('fs').readFileSync;
+const join = require('path').join;
+const test = require('ava');
+const fn = require('../');
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+const file = join(__dirname, 'fixtures', 'foo.js');
+const data = read(file, 'utf8');
+
+const tmp = data
+	.replace('var ', '')
+	.replace(/export /gi, 'exports.')
+	.replace('function foo', 'foo = function');
+
+const output = fn(file, tmp);
+
+test('returns an object', t => t.is(typeof output, 'object'));
+test('returns all keys', t => t.is(Object.keys(output).length, 2));
+test('object key names', t => t.deepEqual(Object.keys(output), ['hello', 'foo']));
+test('typeof output.hello', t => t.is(typeof output.hello, 'string'));
+test('typeof output.foo', t => t.is(typeof output.foo, 'function'));
+test('value of output.hello', t => t.is(output.hello, 'world'));
+test('value of output.foo', t => t.is(output.foo(16, 3), 19));
+
+test('filepath must be defined', t => {
+	const err = t.throws(() => fn(), TypeError);
+	t.is(err.message, 'Filepath must be a string; got undefined');
+});
+
+test('filepath must be string', t => {
+	const err = t.throws(() => fn(123), TypeError);
+	t.is(err.message, 'Filepath must be a string; got number');
+});
+
+test('contents must be defined', t => {
+	const err = t.throws(() => fn('foo'), TypeError);
+	t.is(err.message, 'Contents must be a string; got undefined');
+});
+
+test('contents must be string', t => {
+	const err = t.throws(() => fn('foo', 123), TypeError);
+	t.is(err.message, 'Contents must be a string; got number');
 });
