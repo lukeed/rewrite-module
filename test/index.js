2b4d3f3482a6f9921840101c4bf5e3020a0dd435
Luke Edwards <luke.edwards05@gmail.com>
2017-02-12 01:56:07 -0800

    add test for module.exports = fn

--- test/index.js
+++ test/index.js
@@ -3,23 +3,31 @@ const join = require('path').join;
 const test = require('ava');
 const fn = require('../');
 
-const file = join(__dirname, 'fixtures', 'foo.js');
-const data = read(file, 'utf8');
+const file1 = join(__dirname, 'fixtures', 'foo.js');
+const file2 = join(__dirname, 'fixtures', 'bar.js');
 
-const tmp = data
+const data1 = read(file1, 'utf8');
+const data2 = read(file2, 'utf8');
+
+const tmp1 = data1
 	.replace('var ', '')
 	.replace(/export /gi, 'exports.')
 	.replace('function foo', 'foo = function');
+const tmp2 = data2.replace('FOOBAR', 'bar');
 
-const output = fn(file, tmp);
+const out1 = fn(file1, tmp1);
+const out2 = fn(file2, tmp2);
 
-test('returns an object', t => t.is(typeof output, 'object'));
-test('returns all keys', t => t.is(Object.keys(output).length, 2));
-test('object key names', t => t.deepEqual(Object.keys(output), ['hello', 'foo']));
-test('typeof output.hello', t => t.is(typeof output.hello, 'string'));
-test('typeof output.foo', t => t.is(typeof output.foo, 'function'));
-test('value of output.hello', t => t.is(output.hello, 'world'));
-test('value of output.foo', t => t.is(output.foo(16, 3), 19));
+test('out1: returns an object', t => t.is(typeof out1, 'object'));
+test('out1: returns all keys', t => t.is(Object.keys(out1).length, 2));
+test('out1: key names', t => t.deepEqual(Object.keys(out1), ['hello', 'foo']));
+test('typeof out1.hello', t => t.is(typeof out1.hello, 'string'));
+test('typeof out1.foo', t => t.is(typeof out1.foo, 'function'));
+test('value of out1.hello', t => t.is(out1.hello, 'world'));
+test('value of out1.foo', t => t.is(out1.foo(16, 3), 19));
+
+test('out2: returns a function', t => t.is(typeof out2, 'function'));
+test('value of out2', t => t.is(out2(), 'bar'));
 
 test('filepath must be defined', t => {
 	const err = t.throws(() => fn(), TypeError);
